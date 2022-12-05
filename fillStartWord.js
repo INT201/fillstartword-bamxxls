d7e5d66dd5896d3c4e5ee30f5aa72bb0903145de
bamxxls <88096696+bamxxls@users.noreply.github.com>
2022-12-05 17:39:05 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,13 +1,21 @@
 const { template } = require('@babel/core')
 
-function fillStartWord(startWord, word) {
+// function fillStartWord(startWord, word) {
 
-if(word===null || word===undefined){
-    return undefined;
-}
+// if(word===null || word===undefined){
+//     return undefined;
+// }
 
-let fullword; 
-word.includes(startWord) ? (fullword=word) : (fullword=startWord+word);
-  return fullword;
+// let fullword; 
+// word.includes(startWord) ? (fullword=word) : (fullword=startWord+word);
+//   return fullword;
+
+  if(!word){
+    return undefined
+  }
+  if(word.includes(startWord)){
+    return word
+  }
+  return startWord.concat(word)
 }
 module.exports = fillStartWord
